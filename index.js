9b287e3ce223a5dee45a5f4a2380ca7a475d038b
Hero's Dad <cody.monta@gmail.com>
2020-01-07 13:19:51 -0600

    push

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 import puppeteer from 'puppeteer'
 import dotenv from 'dotenv'
-import nodemailer from 'nodemailer'
 import { CronJob } from 'cron'
 import moment from 'moment'
+import express from 'express'
 
-// '*/10 * * * * *' - to test every ten seconds 
+import { send } from './emailTransporter'
 
-const job = new CronJob('00 00 09 * * 1-5', function() {
-	//  run M-F @ 9am 
-	(async () => {
-			dotenv.config()
-		    const SELECTOR = process.env.SELECTOR
-		    // let transporter = nodemailer.createTransport(transport[, defaults])
-		
-		
-			const browser = await puppeteer.launch()
-			const page = await browser.newPage()
-			await page.goto(process.env.URL)
-			await page.waitForSelector(SELECTOR)
-		
-			const jobs = await page.evaluate(selector => {
-				const jobNodeList = document.querySelectorAll(selector)
-				const jobArr = []
-				jobNodeList.forEach(job => jobArr.push(job.textContent))
-		
-				return jobArr
-			}, SELECTOR)
-			const date = moment().format("MMM Do YY")
+const app = express()
+const PORT = process.env.PORT || 3000
 
-			console.log(date, jobs)
-		
-			await browser.close()
-		})()
+/*
+    '10 * * * * *' 
+    to test every ten seconds
+*/
+/*
+    run M-F @ 9am
+    '00 00 09 * * 1-5'
+*/
+const job = new CronJob('10 * * * * *', function() {
+	//  run M-F @ 9am
+	;(async () => {
+		dotenv.config()
+		const SELECTOR = process.env.SELECTOR
+		// let transporter = nodemailer.createTransport(transport[, defaults])
 
-  });
+		const browser = await puppeteer.launch()
+		const page = await browser.newPage()
+		await page.goto(process.env.URL)
+		await page.waitForSelector(SELECTOR)
 
-job.start()
+		const jobs = await page.evaluate(selector => {
+			const jobNodeList = document.querySelectorAll(selector)
+			const jobArr = []
+			jobNodeList.forEach(job => jobArr.push(job.textContent))
+
+			return jobArr
+		}, SELECTOR)
+		const date = moment().format('MMM Do YY')
+
+		console.log(date, jobs)
+
+		await browser.close()
+	})()
+	send()
+})
+
+app.listen(PORT, () => {
+	console.log(`App listening on http://localhost:${PORT} 🚀`)
+	job.start()
+})
